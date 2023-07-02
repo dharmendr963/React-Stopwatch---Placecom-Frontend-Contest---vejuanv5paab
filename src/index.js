6696ccae50c1c95cbc7b201f32bf5c6c5b86d24e
Dharmendra Kumar <114353361+dharmendr963@users.noreply.github.com>
2023-07-01 23:25:03 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css';
-import App from "./components/App";
-import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 
 
+ReactDOM.render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+  );
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
