7b317af4a6e1bdfb70e9dcff4dead4037f9e77b1
Dharmendra Kumar <114353361+dharmendr963@users.noreply.github.com>
2023-07-12 06:03:21 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
+import React, { useState, useRef } from 'react';
 
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState([]);
+  const intervalRef = useRef(null);
 
-ReactDOM.render(
-  <React.StrictMode>
-  <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  const startTimer = () => {
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 10);
+    }, 10);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  const resetTimer = () => {
+    clearInterval(intervalRef.current);
+    setTime(0);
+    setLaps([]);
+  };
+
+  const lapTimer = () => {
+    setLaps((prevLaps) => [...prevLaps, time]);
+  };
+
+  return (
+    <div>
+      <h2>Stopwatch</h2>
+      <p>{(time / 1000).toFixed(3)}</p>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
+      <button onClick={lapTimer}>Lap</button>
+      {laps.length > 0 && (
+        <div className="lap-section">
+          {laps.map((lapTime, index) => (
+            <p key={index}>{(lapTime / 1000).toFixed(3)}</p>
+          ))}
+        </div>
+      )}
+    </div>
   );
+};
 
+export default Stopwatch;
